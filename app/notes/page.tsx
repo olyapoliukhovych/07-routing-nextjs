5a6bd724c8014b7d45754d1dc678f425890ae64f
Olya Poliukhovych <olha.poliukhovych.1@gmail.com>
2026-01-14 00:53:18 +0200

    change page.tsx

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,18 +1,5 @@
-import { fetchNotes } from '@/lib/api';
-import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
-import NotesClient from './filter/[...slug]/Notes.client';
+import { redirect } from 'next/navigation';
 
-export default async function NotesPage() {
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: ['notes', 1, ''],
-    queryFn: () => fetchNotes(1, ''),
-  });
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <NotesClient />
-    </HydrationBoundary>
-  );
+export default function NotesPage() {
+  redirect('/notes/filter/all');
 }
